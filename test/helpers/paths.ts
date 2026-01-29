b6e9f66c14b84565d1f4dbbf73076d8e658b884c
Steve <steve@steve-adams.me>
2026-01-28 23:33:41 -0800

    Remove unused test data, paths

--- test/helpers/paths.ts
+++ test/helpers/paths.ts
@@ -1,5 +1,3 @@
 import { join } from "@std/path";
 
-export const TEST_DIR = join(Deno.cwd(), "test");
-export const TEST_DATA_DIR = join(TEST_DIR, "data");
-export const TEST_CONFIG_DIR = join(TEST_DIR, "example-config");
+export const TEST_CONFIG_DIR = join(Deno.cwd(), "test", "example-config");
